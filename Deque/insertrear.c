6a1bf5ac3ad858a9c54127973c2167805691a384
TartejBrothers <taranjeetsinghbedi2@gmail.com>
2023-12-30 16:34:11 +0530

    Deque Insert Rear

---
+++ Deque/insertrear.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+# define n 10
+int deque[n], front=-1,rear=-1;
+void insert_rear(){
+    int element;
+    printf("Enter Element : ");
+    scanf("%d",&element);
+    if((front==0 && rear==n-1) || (front==rear+1))
+    {
+        printf("Overflow");
+    }
+      else if((front==-1) && (rear==-1))    
+    {    
+        rear=0;    
+        deque[rear]=element;    
+    }    
+    else if(rear==n-1)    
+    {    
+        rear=0;    
+        deque[rear]=element;    
+    }    
+    else    
+    {    
+        rear=rear+1;
+        deque[rear]=element;    
+    }    
+}
+void display() {
+    if (front == -1 && rear == -1) {
+        printf("\nDeque is empty\n");
+    }
+    printf("\nElements in a deque are:\n");
+    for (int i = front+1; i <= rear; i++) {
+        printf("Element: %d\n", deque[i]);
+    }
+}
+int main(){
+    int num;
+    printf("Enter Number of Elements : ");
+    scanf("%d",&num);
+    for(int i=0;i<num;i++){
+        insert_rear();
+    }
+    display();
+}
